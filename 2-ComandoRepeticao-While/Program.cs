7de4139defddc0b95385de523310e1c08cf9c312
unknown <ulisses.afc@gmail.com>
2023-01-05 22:52:08 -0300

    segunda solução

---
+++ 2-ComandoRepeticao-While/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2_ComandoRepeticao_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabuada;
+            Console.Write("Qual a tabuada?");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+            string valor = string.Empty;
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}",tabuada, i, i * tabuada);
+            }
+        }
+    }
+}
---
+++ Exerc01/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Exerc01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quero Cadastrar 2 alunos e suas duas notas
+            //Quero calcular a média destas notas
+            //Se média > 6 = aprovado senão reprovado
+
+            string aluno1 = "Ulisses";
+            string aluno2 = "Campos";
+
+            double a1n1 = 8;
+            double a1n2 = 8;
+            double a2n1 = 4;
+            double a2n2 = 4;
+
+            double ma1 = (a1n1 + a1n2)/2;
+            double ma2 = (a2n1 + a2n2)/2;
+
+            if (ma1 > 6)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else                
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+            if (ma2 > 6) 
+            {
+                Console.WriteLine("Aprovado");
+            } 
+            else 
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+            Console.WriteLine($"Média aluno: {aluno1} - {ma1}");
+            Console.WriteLine($"Média aluno: {aluno2} - {ma2}");
+
+
+        }
+    }
+}
